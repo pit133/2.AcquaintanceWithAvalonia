01191c576fa494cc17d28becf48e22bc3fbf820e
pit133 <117358265+pit133@users.noreply.github.com>
2024-03-03 21:06:22 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DZ2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
