64623908f81fa57b02e70f7befc4359f7c71c2ad
pit133 <117358265+pit133@users.noreply.github.com>
2024-03-04 16:18:51 +0700

    добавил обработчик нажатия на кнопки, добавил метод красящий прямоугольник в цвет кнопки

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,10 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using System;
+using System.Diagnostics;
+
 
 namespace DZ2
 {
@@ -7,6 +13,46 @@ namespace DZ2
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+            
+
+            //var rectangle = this.FindControl<Avalonia.Controls.Shapes.Rectangle>("ColorRectangle");
+
+            //var button = this.FindControl<Button>("buttonKhaki");
+            //buttonKhaki.Click += ButtonKhaki_Click;
+
+        }
+
+        private void Button_Click(object? sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                // Получаем имя кнопки
+                string buttonName = button.Name;
+                FillRectangle(buttonName);
+            }
+
+
+        }
+        private void FillRectangle(string buttonName)
+        {
+            var button = this.FindControl<Button>(buttonName);
+            var rectangle = this.FindControl<Avalonia.Controls.Shapes.Rectangle>("ColorRectangle");
+            var backgroundBrush = button.Background;
+
+            if (backgroundBrush is ISolidColorBrush solidColorBrush)
+            {
+                var color = solidColorBrush.Color;
+                rectangle.Fill = new SolidColorBrush(color);
+            }
+        }
+
+        
     }
 }
